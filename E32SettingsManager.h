a03e68ba76748f174f9ab9cd3877b58c9b09c40f
exit32studio <contact@exit32studio.com>
2017-11-25 23:28:28 -0500

    Comments
    Added comments to header file

--- E32SettingsManager.h
+++ E32SettingsManager.h
@@ -8,10 +8,20 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
+/*
+ */
+
 @interface E32SettingsManager : NSObject <UITableViewDataSource>
 
+//The settings themselves are housed in a dictionary.  Note that the dictionary is readonly.  You must use the changeSettingNamed:toValue method to change settings
 @property (readonly, strong, nonatomic) NSDictionary *settings;
+
+/*
+ *** DESIGNATED INITIALIZER ***
+ */
 +(id)initWithName:(NSString *)name;
+
+//The following methods will return a boolean value to let consumers know if the method was successful
 -(bool)changeSettingNamed:(NSString *)name toValue:(NSObject *)value;
 -(bool)saveSettings;
 
